39eb37fc9fc9ab0c8cf6f70e5f28605c45c55674
Anshul Choudhary <anshulch366@gmail.com>
2026-09-02 15:21:44 +0530

    2sep

--- script.js
+++ script.js
@@ -1,17 +1,81 @@
 // Question 1 
 
-function calculateNNumber(num1, num2) {
-    return num1 + num2;
+// function calculateBalance(balance, withdrawalAmount) {
+//     return balance - withdrawalAmount;
+// }
+
+// let balance = 1000;
+// let withdrawalAmount = 500;
+
+// if (withdrawalAmount > balance  ) {
+//     console.log("Insufficient funds");
+// }
+
+//     else if (balance > withdrawalAmount){
+//         let remainingBalance = calculateBalance(balance, withdrawalAmount);
+//         console.log("Total amount : " + balance);
+//         console.log("Withdrawal amount: " + withdrawalAmount);
+//         console.log("Remaining balance: " + remainingBalance);
+//     }
+
+
+
+
+
+
+// Question 2
+
+function shoppingBill(productName, productPrice, quantity, premiumCustomer){
+    let totalPrice = productPrice * quantity;
+    let discount = 0;
+    let premiumCustomerDiscount = 0;
+
+
+if(quantity === 0){
+    console.log("Please select atleast 1 quantity");
 }
 
-num1 = Number(prompt("Enter your account balance"));
-num2 = Number(prompt("Enter Withdrwal amount"));
+if(quantity === 6){
+    discount = 5000;
+}
 
-if (num2 > num1) {
-    alert("Insufficient funds");
-    console.log("Insufficient funds");
+ if (premiumCustomer === "yes"){
+premiumCustomerDiscount = 5000;
+}
 
-    if (num1 > num2){
-        console.log("Remaining balance: " + (num1 - num2));
+    let finalAmount = totalPrice - discount - premiumCustomerDiscount;
+    return {
+        discount = discount,
+        premiumCustomerDiscount = premiumCustomerDiscount,
+        finalAmount = finalAmount
+
     }
+}
+
+let productName = "Asus laptop";
+let productPrice = 20000;
+let quantity = 6;
+let premiumCustomer = "yes";
+
+
+
+
+
+if (quantity >= 1){
+
+       let bill = shoppingBill(
+        productName,
+        productPrice,
+        quantity,
+        premiumCustomer
+    );
+
+    console.log("----Total bill----");
+    console.log("Product name; " + productName);
+    console.log("total price: " + productPrice);
+    console.log("Total quantity: " + quantity);
+    console.log("Premium Customer? : " + premiumCustomer);
+    console.log("Premium customer discount: " + bill.premiumCustomerDiscount)
+    console.log("Additional disctount " + bill.discount);
+    console.log("Final amount to pay: " + bill.finalAmount)
 }
